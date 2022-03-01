faec9064c227fedd7c4b8bfafdd6d5e0b766ea3e
Shanghua Gao <shgao@live.com>
2022-03-01 17:13:50 +0800

    update unzip file name

--- datapreparation/datapreparation_anno.sh
+++ datapreparation/datapreparation_anno.sh
@@ -6,27 +6,27 @@ cd $1
 if [ "$2" = "50" ]
 then
   wget ${ImageNetS50}
-  unzip ${ImageNetS50}
+  unzip ImageNetS50-a0fe9d82231f9bc4787ee76e304dfa51.zip
 fi
 
 if [ "$2" = "300" ]
 then
   wget ${ImageNetS300}
-  unzip ${ImageNetS300}
+  unzip ImageNetS300-dd8e42e156bac415a35c3c239de297bf.zip
 fi
 
 if [ "$2" = "919" ]
 then
   wget ${ImageNetS919}
-  unzip ${ImageNetS919}
+  unzip ImageNetS919-5f7f58ae1003d21da9409a8576bf7680.zip
 fi
 
 if [ "$2" = "all" ]
 then
   wget ${ImageNetS50}
-  unzip ${ImageNetS50}
+  unzip ImageNetS50-a0fe9d82231f9bc4787ee76e304dfa51.zip
   wget ${ImageNetS300}
-  unzip ${ImageNetS300}
+  unzip ImageNetS300-dd8e42e156bac415a35c3c239de297bf.zip
   wget ${ImageNetS919}
-  unzip ${ImageNetS919}
+  unzip ImageNetS919-5f7f58ae1003d21da9409a8576bf7680.zip
 fi
